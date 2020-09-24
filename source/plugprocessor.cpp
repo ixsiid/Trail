cd582e7762e81c50af95d0c14ef377f3b997ce1d
unknown <tal@HA000102600.mcdiv.csg.kyocera.co.jp>
2020-09-24 13:48:25 +0900

    fix

--- source/plugprocessor.cpp
+++ source/plugprocessor.cpp
@@ -1,39 +1,3 @@
-//------------------------------------------------------------------------
-// Project     : VST SDK
-//
-// Category    : Examples
-// Filename    : plugprocessor.cpp
-// Created by  : Steinberg, 01/2018
-// Description : HelloWorld Example for VST 3
-//
-//-----------------------------------------------------------------------------
-// LICENSE
-// (c) 2020, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//   * Redistributions of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//   * Neither the name of the Steinberg Media Technologies nor the names of its
-//     contributors may be used to endorse or promote products derived from this
-//     software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
-
 #include "../include/plugprocessor.h"
 
 #include <base/source/fstreamer.h>
@@ -125,7 +89,7 @@ tresult PLUGIN_API PlugProcessor::setActive(TBool state) {
 		message->getAttributes()->setInt(u8"PARAMETER", (int64)&initializeMessage);
 		sendMessage(message);
 	} else {	// Release
-		// Free Memory if still allocated
+			// Free Memory if still allocated
 	}
 
 	return AudioEffect::setActive(state);
@@ -198,7 +162,7 @@ tresult PLUGIN_API PlugProcessor::process(Vst::ProcessData& data) {
 		event.midiCCOut.value		= 127 - intValue;
 		event.midiCCOut.value2		= 0;
 
-		data.outputEvents->addEvent(event);
+		if (data.outputEvents) data.outputEvents->addEvent(event);
 
 		value /= 512.0;
 		value = 1.0 - value;
@@ -211,13 +175,15 @@ tresult PLUGIN_API PlugProcessor::process(Vst::ProcessData& data) {
 		int32 queueIndex = 0;
 		tresult r;
 
-		r = data.outputParameterChanges
-		    ->addParameterData(HelloWorldParams::kParamFp, paramIndex)
-		    ->addPoint(0, value, queueIndex);
-		    
-		r = data.outputParameterChanges
-		    ->addParameterData(HelloWorldParams::kParamF0, paramIndex)
-		    ->addPoint(0, dft->f0 * 8192 / 44100, queueIndex);
+		if (data.outputParameterChanges) {
+			r = data.outputParameterChanges
+				   ->addParameterData(HelloWorldParams::kParamFp, paramIndex)
+				   ->addPoint(0, value, queueIndex);
+
+			r = data.outputParameterChanges
+				   ->addParameterData(HelloWorldParams::kParamF0, paramIndex)
+				   ->addPoint(0, dft->f0 * 8192 / 44100, queueIndex);
+		}
 	}
 	return kResultOk;
 }
@@ -241,7 +207,6 @@ tresult PLUGIN_API PlugProcessor::setState(IBStream* state) {
 	float _noise_level;
 	if (!streamer.readFloat(_noise_level)) return kResultFalse;
 	dft->noise_level = _noise_level;
-
 
 	return kResultOk;
 }
